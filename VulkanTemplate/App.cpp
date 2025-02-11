86881fd786b088d647ddb0f5e2f3ec2fed8cf627
Rain <33805016+Rain1950@users.noreply.github.com>
2025-02-11 21:04:42 +0100

    Vertex buffer Integration

---
+++ VulkanTemplate/App.cpp
@@ -0,0 +1,116 @@
+#include "App.h"
+
+
+void App::Run() {
+	windowManager->InitWindow();
+	glfwSetWindowUserPointer(windowManager->window, &physicalDeviceManager);
+	InitVulkan();
+	MainLoop();
+	Cleanup();
+}
+
+void App::InitVulkan() {
+	vulkanInstance->CreateInstance();
+	windowManager->CreateSurface();
+	physicalDeviceManager.PickPhysicalDevice();
+	logicalDeviceManager.CreateLogicalDevice(&physicalDeviceManager, &validationLayersManager);
+	physicalDeviceManager.CreateSwapChain(logicalDeviceManager.device);
+	physicalDeviceManager.CreateImageViews(logicalDeviceManager.device);
+	graphicsPipelineManager.CreateRenderPass(physicalDeviceManager.swapChainImageFormat, logicalDeviceManager.device);
+	graphicsPipelineManager.CreateGraphicsPipeline(logicalDeviceManager.device);
+	physicalDeviceManager.CreateFrameBuffers(logicalDeviceManager.device, graphicsPipelineManager.renderPass);
+	physicalDeviceManager.CreateCommandPool(logicalDeviceManager.device);
+	graphicsPipelineManager.CreateVertexBuffer(logicalDeviceManager.device,physicalDeviceManager.physicalDevice);
+	physicalDeviceManager.CreateCommandBuffers(logicalDeviceManager.device);
+	physicalDeviceManager.CreateSyncObjects(logicalDeviceManager.device);
+
+}
+
+void App::DrawFrame()
+{
+	vkWaitForFences(logicalDeviceManager.device, 1, &physicalDeviceManager.inFlightFences[physicalDeviceManager.currentFrame], VK_TRUE, UINT64_MAX);
+
+	uint32_t imageIndex;
+	VkResult result = vkAcquireNextImageKHR(logicalDeviceManager.device, physicalDeviceManager.swapChain, UINT64_MAX, physicalDeviceManager.imageAvailableSemaphores[physicalDeviceManager.currentFrame], VK_NULL_HANDLE, &imageIndex);
+	if (result == VK_ERROR_OUT_OF_DATE_KHR) {
+		physicalDeviceManager.RecreateSwapChain(logicalDeviceManager.device, graphicsPipelineManager.renderPass);
+		return;
+	}
+	else if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+		throw std::runtime_error("Failed to acquire swap chain images");
+	}
+
+	vkResetFences(logicalDeviceManager.device, 1, &physicalDeviceManager.inFlightFences[physicalDeviceManager.currentFrame]);
+
+	vkResetCommandBuffer(physicalDeviceManager.commandBuffers[physicalDeviceManager.currentFrame], 0);
+	physicalDeviceManager.RecordCommandBuffer(physicalDeviceManager.commandBuffers[physicalDeviceManager.currentFrame], imageIndex, graphicsPipelineManager);
+
+	VkSubmitInfo submitInfo{};
+	submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+	VkSemaphore waitSemaphores[] = { physicalDeviceManager.imageAvailableSemaphores[physicalDeviceManager.currentFrame] };
+	VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
+	submitInfo.waitSemaphoreCount = 1;
+	submitInfo.pWaitSemaphores = waitSemaphores;
+	submitInfo.pWaitDstStageMask = waitStages;
+	submitInfo.commandBufferCount = 1;
+	submitInfo.pCommandBuffers = &physicalDeviceManager.commandBuffers[physicalDeviceManager.currentFrame];
+
+	VkSemaphore signalSemaphores[] = { physicalDeviceManager.renderFinishedSemaphores[physicalDeviceManager.currentFrame] };
+	submitInfo.signalSemaphoreCount = 1;
+	submitInfo.pSignalSemaphores = signalSemaphores;
+
+	if (vkQueueSubmit(logicalDeviceManager.graphicsQueue, 1, &submitInfo, physicalDeviceManager.inFlightFences[physicalDeviceManager.currentFrame]) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to submit draw command buffer!");
+	}
+
+	VkPresentInfoKHR presentInfo{};
+	presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+	presentInfo.waitSemaphoreCount = 1;
+	presentInfo.pWaitSemaphores = signalSemaphores;
+	VkSwapchainKHR swapChains[] = { physicalDeviceManager.swapChain };
+	presentInfo.swapchainCount = 1;
+	presentInfo.pSwapchains = swapChains;
+	presentInfo.pImageIndices = &imageIndex;
+
+	result = vkQueuePresentKHR(logicalDeviceManager.presentQueue, &presentInfo);
+
+	if (result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || physicalDeviceManager.frameBufferResized) {
+		physicalDeviceManager.frameBufferResized = false;
+		physicalDeviceManager.RecreateSwapChain(logicalDeviceManager.device, graphicsPipelineManager.renderPass);
+	}
+	else if (result != VK_SUCCESS) {
+		throw std::runtime_error("Failed to present swap chain image");
+	}
+
+	physicalDeviceManager.currentFrame = (physicalDeviceManager.currentFrame + 1) % physicalDeviceManager.MAX_FRAMES_IN_FLIGHT;
+
+
+
+}
+
+void App::MainLoop()
+{
+	while (!glfwWindowShouldClose(windowManager->window)) {
+		glfwPollEvents();
+		DrawFrame();
+	}
+	vkDeviceWaitIdle(logicalDeviceManager.device);
+
+}
+
+void App::Cleanup()
+{
+	physicalDeviceManager.CleanupSyncObjects(logicalDeviceManager.device);
+	physicalDeviceManager.CleanupCommandPool(logicalDeviceManager.device);
+	physicalDeviceManager.CleanupFrameBuffers(logicalDeviceManager.device);
+	graphicsPipelineManager.CleanGraphicsPipeline(logicalDeviceManager.device);
+	graphicsPipelineManager.CleanPipelineLayout(logicalDeviceManager.device);
+	graphicsPipelineManager.CleanRenderPass(logicalDeviceManager.device);
+	physicalDeviceManager.CleanupImageViews(logicalDeviceManager.device);
+	physicalDeviceManager.CleanupSwapChain(logicalDeviceManager.device, physicalDeviceManager.swapChain);
+	graphicsPipelineManager.CleanupVertexBuffer(logicalDeviceManager.device);
+	logicalDeviceManager.Cleanup();
+	windowManager->Cleanup();
+	vulkanInstance->Cleanup();
+}
+
--- VulkanTemplate/GraphicsPipelineManager.cpp
+++ VulkanTemplate/GraphicsPipelineManager.cpp
@@ -63,6 +63,56 @@ void GraphicsPipelineManager::CreateRenderPass(VkFormat swapChainImageFormat, Vk
 	
 }
 
+uint32_t GraphicsPipelineManager::FindMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties, VkPhysicalDevice& physicalDevice) {
+	VkPhysicalDeviceMemoryProperties memProperties;
+	vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+	for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+		if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) return i;
+	}
+	throw std::runtime_error("Failed to find suitable memory type!");
+
+}
+
+void GraphicsPipelineManager::CreateVertexBuffer(VkDevice& device,VkPhysicalDevice& physicalDevice)
+{
+	VkBufferCreateInfo bufferInfo{};
+	bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	bufferInfo.size = sizeof(vertices[0]) * vertices.size();
+	bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+	bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to create vertex buffer");
+	}
+
+	VkMemoryRequirements memRequirements;
+	vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
+
+	VkMemoryAllocateInfo allocInfo{};
+	allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	allocInfo.allocationSize = memRequirements.size;
+	allocInfo.memoryTypeIndex = FindMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,physicalDevice);
+
+	if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to allocate vertex buffer memory!");
+	}
+
+	vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+
+	void* data;
+	vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
+	memcpy(data, vertices.data(), (size_t)bufferInfo.size);
+	vkUnmapMemory(device, vertexBufferMemory);
+
+
+}
+
+void GraphicsPipelineManager::CleanupVertexBuffer(VkDevice& device) {
+	vkDestroyBuffer(device, vertexBuffer, nullptr);
+	vkFreeMemory(device, vertexBufferMemory, nullptr);
+
+}
+
 
 GraphicsPipelineManager::GraphicsPipelineManager(VkExtent2D SwapChainExtent) : swapChainExtent{ SwapChainExtent } {};
 
@@ -99,10 +149,15 @@ void GraphicsPipelineManager::CreateGraphicsPipeline(VkDevice& device)
 	dynamicState.pDynamicStates = dynamicStates.data();
 
 
+	auto bindingDescription = Vertex::GetBindingDescription();
+	auto attributeDescriptions = Vertex::GetAttributeDescriptions();
+
 	VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
 	vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-	vertexInputInfo.vertexBindingDescriptionCount = 0;
-	vertexInputInfo.vertexAttributeDescriptionCount = 0;
+	vertexInputInfo.vertexBindingDescriptionCount = 1;
+	vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+	vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+	vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
 
 	VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
 	inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
